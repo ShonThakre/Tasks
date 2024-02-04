565f086218bf5455330e2c356f15440712738e4a
shon <shoonthakre@gmail.com>
2024-02-04 17:07:23 +0530

    Login and Register DTO

---
+++ Models/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksAPI.Models.DTO
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Password { get; set; }
+    }
+}
