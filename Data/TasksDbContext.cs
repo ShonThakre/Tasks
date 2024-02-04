e40aaa92a23ec7171714b56eb3d2fa86a982b512
shon <shoonthakre@gmail.com>
2024-02-04 14:33:41 +0530

    Add project files.

---
+++ Data/TasksDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TasksAPI.Data
+{
+    public class TasksDbContext : DbContext
+    {
+        public TasksDbContext(DbContextOptions options): base(options) 
+        {
+            
+        }
+    }
+}
