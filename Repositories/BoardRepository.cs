00d51d1b39d31752a7b567090d1f94b61ff739f2
shon <shoonthakre@gmail.com>
2024-02-05 21:32:29 +0530

    board repository created

---
+++ Repositories/BoardRepository.cs
@@ -0,0 +1,38 @@
+﻿using TasksAPI.Data;
+using TasksAPI.Models.Domain;
+
+namespace TasksAPI.Repositories
+{
+    public class BoardRepository : IBoardRepository
+    {
+        private readonly TasksDbContext _Context;
+
+
+        public BoardRepository(TasksDbContext tasksDbContext)
+        {
+            _Context = tasksDbContext;
+        }
+
+        public async Task<Board> CreateAsync(Board board)
+        {
+           await _Context.Boards.AddAsync(board);
+            await _Context.SaveChangesAsync();
+            return board;
+        }
+
+        public Task<Board?> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Board>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Board?> UpdateAsync(Guid id, Board board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/IBoardRepository.cs
@@ -0,0 +1,12 @@
+﻿using TasksAPI.Models.Domain;
+
+namespace TasksAPI.Repositories
+{
+    public interface IBoardRepository
+    {
+        Task<List<Board>> GetAllAsync();
+        Task<Board> CreateAsync(Board board);
+        Task<Board?> UpdateAsync(Guid id, Board board);
+        Task<Board?> DeleteAsync(Guid id);
+    }
+}
