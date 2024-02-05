256e9e65063fea48d4ef2bc10b5f1bfe7b16538c
shon <shoonthakre@gmail.com>
2024-02-05 21:32:58 +0530

    Board controller GetAll()

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TasksAPI.Models.Domain;
+using TasksAPI.Models.DTO;
+using TasksAPI.Repositories;
+
+namespace TasksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : ControllerBase
+    {
+        private readonly BoardRepository _boardRepository;
+        private readonly IMapper _mapper;
+
+        public BoardController(BoardRepository boardRepository, IMapper mapper)
+        {
+            _boardRepository = boardRepository;
+            _mapper = mapper;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() {
+
+            var BoardsDomain = await _boardRepository.GetAllAsync();
+
+           var boardDto = _mapper.Map<List<BoardDTO>>(BoardsDomain);
+
+            return Ok(boardDto);
+
+        }
+    }
+}
