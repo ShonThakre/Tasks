672fd1ff2dc68bfdac23eeddf0df5470b3769b32
shon <shoonthakre@gmail.com>
2024-02-05 21:33:51 +0530

    BoardDTO created

---
+++ Models/DTO/BoardDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksAPI.Models.DTO
+{
+    public class BoardDTO
+    {
+        public int Id { get; set; }
+ 
+        public string Title { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime EntryDate { get; set; } 
+
+        public string UserId { get; set; } 
+    }
+}
