b05207d10ea26cb0f952c2e4e70b1f0b2b33665a
shon <shonthakre@gmail.com>
2024-02-06 11:35:52 +0530

    tasklist request dto

---
+++ Models/DTO/TaskListRequestDTO .cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksAPI.Models.DTO
+{
+    public class TaskListRequestDTO
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int Sequence { get; set; }
+
+        [Required]
+        public int BoardId { get; set; } 
+
+    }
+}
