e4eb405c5fab795b2785fb48b3313311c1ac7fcc
shon <shoonthakre@gmail.com>
2024-02-06 09:10:56 +0530

    taskList DTO created

---
+++ Models/DTO/TaskListDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksAPI.Models.DTO
+{
+    public class TaskListDTO
+    {
+        public int Id { get; set; }    
+
+        public string Title { get; set; }
+
+        public string Status { get; set; } = "Y";
+
+        public int Sequence { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public int BoardId { get; set; } 
+
+    }
+}
