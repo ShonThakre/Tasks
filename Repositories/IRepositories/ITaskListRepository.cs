fa535119f6128ee088d7ca58814566abbc6f5dc1
shon <shoonthakre@gmail.com>
2024-02-06 08:20:54 +0530

    tasklist interface modified

---
+++ Repositories/IRepositories/ITaskListRepository.cs
@@ -0,0 +1,14 @@
+﻿using TasksAPI.Models.Domain;
+
+namespace TasksAPI.Repositories.IRepositories
+{
+    public interface ITaskListRepository
+    {
+        Task<List<TaskList>> GetAllAsync(int boardId);
+        Task<TaskList> CreateAsync(TaskList taskList);
+        Task<TaskList?> UpdateAsync(int id,TaskList taskList);
+        Task<TaskList?> DeleteAsync(int id);
+        Task<TaskList?> GetAsync(int id);
+
+    }
+}
