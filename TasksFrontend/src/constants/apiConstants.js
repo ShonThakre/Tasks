6c345d7e318ee77fc3d55a7ed2873da2a6f23206
shon <shonthakre@gmail.com>
2024-09-30 21:00:08 +0530

    Adding env and login.

---
+++ TasksFrontend/src/constants/apiConstants.js
@@ -0,0 +1,1 @@
+export const BASE_API_URL = import.meta.env.VITE_Tasks_API_URL;
--- TasksFrontend/src/pages/Login/Login.jsx
+++ TasksFrontend/src/pages/Login/Login.jsx
@@ -1,7 +1,90 @@
+import { useState } from "react";
+import { BASE_API_URL } from "../../constants/apiConstants";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitButton = () => {
+    alert(BASE_API_URL);
+  };
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-300">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold text-gray-900">Sign in</h2>
+        </div>
+
+        <div className="rounded-md shadow-sm">
+          <div className="mb-4">
+            <label htmlFor="email" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="Username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+            />
+            <label
+              htmlFor="remember-me"
+              className="block ml-2 text-sm text-gray-900"
+            >
+              Remember me
+            </label>
+          </div>
+
+          <div className="text-sm">
+            <a
+              href="#"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <button
+            onClick={handleSubmitButton}
+            className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
